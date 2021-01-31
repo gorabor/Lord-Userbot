f5fec7a7840276c200dac38553cf6554bfdc955a
Alvin <75377410+Zora24@users.noreply.github.com>
2021-02-01 00:15:06 +0700

    Create alvin24.py

---
+++ userbot/modules/alvin24.py
@@ -0,0 +1,69 @@
+# Alvin Gans
+
+from time import sleep
+from userbot import CMD_HELP, bot
+from userbot.events import register
+from telethon import events
+import asyncio
+
+
+@register(outgoing=True, pattern="^.hua$")
+async def koc(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("أ‿أ")
+        await e.edit("╥﹏╥")
+        await e.edit("(;﹏;)")
+        await e.edit("(ToT)")
+        await e.edit("(┳Д┳)")
+        await e.edit("(ಥ﹏ಥ)")
+        await e.edit("（；へ：）")
+        await e.edit("(T＿T)")
+        await e.edit("（πーπ）")
+        await e.edit("(Ｔ▽Ｔ)")
+        await e.edit("(⋟﹏⋞)")
+        await e.edit("（ｉДｉ）")
+        await e.edit("(´Д⊂ヽ")
+        await e.edit("(;Д;)")
+        await e.edit("（>﹏<）")
+        await e.edit("(TдT)")
+        await e.edit("(つ﹏⊂)")
+        await e.edit("༼☯﹏☯༽")
+        await e.edit("(ノ﹏ヽ)")
+        await e.edit("(ノAヽ)")
+        await e.edit("(╥_╥)")
+        await e.edit("(T⌓T)")
+        await e.edit("(༎ຶ⌑༎ຶ)")
+        await e.edit("(☍﹏⁰)｡")
+        await e.edit("(ಥ_ʖಥ)")
+        await e.edit("(つд⊂)")
+        await e.edit("(≖͞_≖̥)")
+        await e.edit("(இ﹏இ`｡)")
+        await e.edit("༼ಢ_ಢ༽")
+        await e.edit("༼ ༎ຶ ෴ ༎ຶ༽")
+        
+ 
+@register(outgoing=True, pattern='^.nah(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`\n(\_/)`"
+                     "`\n(●_●)`"
+                     "`\n />💖 *Ini Buat Kamu`")
+    sleep(2)
+    await typew.edit("`\n(\_/)`"
+                     "`\n(●_●)`"
+                     "`\n💖<\  *Tapi Bo'ong`")       
+
+
+  CMD_HELP.update({
+    "animasi":
+    "`.nah`\
+    \nUsage: tapi bo'ong.\
+    \n\n`.ily`\
+    \nUsage: i love you\
+    \n\n`.hua`\
+    \nUsage: Nangis.\
+    \n\n`kosong`\
+    \nUsage: kosong\
+    \n\n`kosong`\
+    \nUsage: kosong"
+})
